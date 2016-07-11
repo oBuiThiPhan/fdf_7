36ec4e9fbacb0fd43f3146f811fab80d6565fec5
phanbt58 <phanbt95@gmail.com>
2016-07-11 21:39:54 +0700

    Show details a product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,12 @@ class ProductsController < ApplicationController
     @products = Product.order("price")
       .paginate page: params[:page], per_page: Settings.per_page
   end
+
+  def show
+    @product = Product.find_by id: params[:id]
+    unless @product
+      flash[:danger] = t "products.show.noproduct"
+      redirect_to products_path
+    end
+  end
 end
