a5220519cfd46aa5baab93cce1d49e0cc1a4f304
HoangLanh <hoanglanhtb@gmail.com>
2016-07-12 14:08:19 +0700

    task18-add-to-cart

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users
   root "static_pages#home"
+  get "/cart" =>"cart#index"
+  get "/cart/clear" => "cart#destroy"
+  get "/cart/:id" =>"cart#create"
   get "help" => "static_pages#help"
   get "about" => "static_pages#about"
   get "contact" => "static_pages#contact"
 
   resources :users, only: [:show, :edit, :update]
 
+
   resources :products, only: [:index, :show] do
     resources :comments, except: [:index, :destroy, :show]
   end
+  resources :cart, only: [:index]
 
   namespace :admin do
     resources :categories, except: :show
