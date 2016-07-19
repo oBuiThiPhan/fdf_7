ce63c1d3070b4560ae7142886c74df39b3523d03
phanbt58 <phanbt95@gmail.com>
2016-07-19 09:58:17 +0700

    fix bug 3

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -34,7 +34,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process resize_to_fit: [200, 150]
+    process resize_to_fill: [200, 150]
   end
 
   # Add a white list of extensions which are allowed to be uploaded.
